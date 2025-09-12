8e9714b52c7e2afbc03b4e942685927701baf0ff
Rolando Velez <rolandovlz@gmail.com>
2025-09-12 12:54:52 -0700

    added initial htmlnode

---
+++ src/test_htmlnode.py
@@ -0,0 +1,12 @@
+import unittest
+
+from htmlnode import HTMLNode
+
+class TestHTMLNode(unittest.TestCase):
+    def test_props_to_html(self):
+        node = HTMLNode(props={"href": "https://www.google.com", "target": "_blank"})
+        expected = ' href="https://www.google.com" target="_blank"'
+        self.assertEqual(node.props_to_html(), expected)
+
+if __name__ == "__main__":
+    unittest.main()
