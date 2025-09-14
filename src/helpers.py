e9117948112b19c26292ea0b3fa3e5b4fe8d3653
Rolando Velez <rolandovlz@gmail.com>
2025-09-13 17:09:20 -0700

    added helper functions and more tests

---
+++ src/helpers.py
@@ -0,0 +1,37 @@
+from textnode import TextType, TextNode
+from htmlnode import LeafNode
+
+def text_node_to_html_node(text_node):
+    match(text_node.text_type):
+        case TextType.TEXT:
+            return LeafNode(None, text_node.text)
+        case TextType.BOLD:
+            return LeafNode("b", text_node.text)
+        case TextType.ITALIC:
+            return LeafNode("i", text_node.text)
+        case TextType.CODE:
+            return LeafNode("code", text_node.text)
+        case TextType.LINK:
+            return LeafNode("a", text_node.text, { "href": f"{text_node.url}"})
+        case TextType.IMAGE:
+            return LeafNode("img", "", { "src": f"{text_node.url}", "alt": f"{text_node.text}"})
+
+        case _:
+            raise Exception(f"Error: Text type '{text_node.text_type}' not found")
+
+def split_nodes_delimiter(old_nodes, delimiter, text_type):
+    new_nodes = []
+    for node in old_nodes:
+        if node.text_type != TextType.TEXT:
+            new_nodes.append(node)
+            continue
+        split_word = node.text.split(delimiter)
+        if len(split_word) % 2 == 0:
+            raise ValueError("invalid text")
+        for i in range(len(split_word)):
+            if i % 2 == 0:
+                new_nodes.append(TextNode(split_word[i], TextType.TEXT))
+            else:
+                new_nodes.append(TextNode(split_word[i], text_type))
+
+    return new_nodes
--- src/test_textnode.py
+++ src/test_textnode.py
@@ -1,7 +1,7 @@
 import unittest
 
 from textnode import TextNode, TextType
-from textnode_to_htmlnode import text_node_to_html_node
+from helpers import text_node_to_html_node, split_nodes_delimiter
 
 class TestTextNode(unittest.TestCase):
     def test_eq(self):
@@ -37,6 +37,45 @@ class TestTextNode(unittest.TestCase):
         self.assertEqual(html_node.tag, "img")
         self.assertEqual(html_node.value, "")
         self.assertDictEqual(html_node.props, { "src": "https://cdn.steamstatic.com/apps/dota2/images/dota_react/dota_footer_logo.png", "alt": "dota 2 logo" })
+
+    def test_split_delimiter_code(self):
+        node = TextNode("This is text with a `code block` word", TextType.TEXT)
+        new_nodes = split_nodes_delimiter([node], "`", TextType.CODE)
+        expected_nodes = [
+            TextNode("This is text with a ", TextType.TEXT),
+            TextNode("code block", TextType.CODE),
+            TextNode(" word", TextType.TEXT)
+        ]
+        self.assertEqual(new_nodes, expected_nodes)
+
+    def test_split_delimiter_bold(self):
+        node = TextNode("This is text with a **bold block** word", TextType.TEXT)
+        new_nodes = split_nodes_delimiter([node], "**", TextType.BOLD)
+        expected_nodes = [
+            TextNode("This is text with a ", TextType.TEXT),
+            TextNode("bold block", TextType.BOLD),
+            TextNode(" word", TextType.TEXT)
+        ]
+        self.assertEqual(new_nodes, expected_nodes)
+
+    def test_split_delimiter_text_type(self):
+        node = TextNode("This is text with a **bold block** word", TextType.BOLD)
+        node2 = TextNode("This is text with a _italic block_ word", TextType.ITALIC)
+        node3 = TextNode("This is text with a `code block` word", TextType.CODE)
+        node4 = TextNode("This is text with a `code block` word", TextType.LINK)
+        node5 = TextNode("This is text with a `code block` word", TextType.IMAGE)
+
+        new_nodes = split_nodes_delimiter([node, node2, node3, node4, node5], "`", TextType.CODE)
+
+        expected_nodes = [
+            TextNode("This is text with a **bold block** word", TextType.BOLD),
+            TextNode("This is text with a _italic block_ word", TextType.ITALIC),
+            TextNode("This is text with a `code block` word", TextType.CODE),
+            TextNode("This is text with a `code block` word", TextType.LINK),
+            TextNode("This is text with a `code block` word", TextType.IMAGE)
+        ]
+        self.assertEqual(new_nodes, expected_nodes)
+
     
 if __name__ == "__main__":
     unittest.main()
