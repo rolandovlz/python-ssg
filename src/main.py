45d9fe2dbfa00abbced95414cd54bbbb4a560f61
Rolando Velez <rolandovlz@gmail.com>
2025-09-17 18:20:01 -0700

    feat: implement public directory generation

--- src/main.py
+++ src/main.py
@@ -1,7 +1,31 @@
+import os
+import shutil
 from textnode import TextNode, TextType
 
 def main():
-    node = TextNode("This is some anchor text", TextType.LINK_TEXT, "https://rvelez.dev")
-    print(node)
+    # node = TextNode("This is some anchor text", TextType.LINK, "https://rvelez.dev")
+    # print(node)
+    generate_public()
+
+def generate_public(src=None, dest=None):
+    project_root = os.path.dirname(os.path.dirname(__file__))
+    public_path = os.path.join(project_root, "public")
+
+    if os.path.exists(public_path):
+        shutil.rmtree(public_path)
+
+    copy_dir_files(os.path.join(project_root, "static"), public_path)
+
+
+def copy_dir_files(src_dir, dest_dir=None):
+    if not os.path.exists(dest_dir):
+        os.mkdir(dest_dir)
+    for item in os.listdir(src_dir):
+        src_item = os.path.join(src_dir, item)
+        dest_item = os.path.join(dest_dir, item)
+        if os.path.isdir(src_item):
+            copy_dir_files(src_item, dest_item)
+        else:
+            shutil.copy(src_item, dest_item)
 
 main()
